8c2e948efc6892db5d43944256549f7542762279
Arsh Quadri <arshquadri111@gmail.com>
2024-05-18 15:53:30 +0530

    Login structure

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import Onboard from "./components/Onboard";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/onboard" element={<Onboard />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="text-2xl text-red-400">Home</div>;
+};
+
+export default Home;
